1318eb6ecaf01bc92a08f0980c85346446c22b07
Safal Singh <safalsinghgaur@gmail.com>
2021-02-05 14:01:25 +0530

    fix

--- VentureBeat Clone/src/App.js
+++ VentureBeat Clone/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import PageHead from './Component/Jobs/Jobs';
+import Navbar from "./Component/Navbar/Navbar"
 
 function App() {
   return (
     <div className="App">
-      <PageHead/>
+      {/* <Navbar/> */}
     </div>
   );
 }
